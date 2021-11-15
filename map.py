1ea507bc6ce33d90b7b19c1bb61a2149374d170b
Mayra Salazar <mayrasalazar@unitec.edu>
2021-11-14 22:17:49 -0600

    fixing objects

--- map.py
+++ map.py
@@ -19,11 +19,11 @@ class Map:
 
     def addPackage(self, package:Package):
         self.packages.append(package)
-        self.map[package.x][package.y] = package.carret
+        self.map[package.x][package.y] = package.currentCarret
 
     def addObstacle(self, obs:Obstacle):
         self.obs.append(obs)
-        self.map[obs.x][obs.y] = obs.carret
+        self.map[obs.x][obs.y] = obs.currentCarret
 
     def addDestination(self,x:int,y:int, dest:Destination):
         self.destinations.append(dest)
@@ -34,7 +34,7 @@ class Map:
         for x in range(self.lines):
             for y in range (self.columns):
                 if y <self.columns-1:
-                    stringMap+= self.map[x][y]
+                    stringMap+= str(self.map[x][y])
                 else:
-                    stringMap+= self.map[x][y]+"\n"
+                    stringMap+= str(self.map[x][y])+"\n"
         return stringMap
